6f92dd50ad60a97f148c0f0d8b3948fa8a835afb
educasotti <educasotti@gmail.com>
2015-06-15 18:51:24 -0300

    bycountry

---
+++ WWW/Controllers/ByCountryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WWW.Controllers
+{
+    public class ByCountryController : Controller
+    {
+        //
+        // GET: /ByCountry/
+
+        public ActionResult Index(string code)
+        {
+            if (code == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+    }
+}
